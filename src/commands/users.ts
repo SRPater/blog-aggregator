a735dbb7828afb132b067e313b2bbb38fca76bfb
SRPater <srpater@gmail.com>
2025-08-21 12:47:27 +0200

    Working on CH2: Database, L3: Drizzle Queries

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,10 +1,29 @@
 import { setUser } from "../config";
+import { createUser, getUserByName } from "src/lib/db/queries/users";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
     if (args.length === 0) {
         throw new Error("Username is required");
     }
 
     setUser(args[0]);
     console.log("User set");
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+    if (args.length === 0) {
+        throw new Error("Username is required");
+    }
+
+    if (await getUserByName(args[0])) {
+        throw new Error("User already exists");
+    }
+
+    const newUser = await createUser(args[0]);
+    setUser(newUser.name);
+    console.log("User was created:");
+    console.log(`id: ${newUser.id}`);
+    console.log(`created at: ${newUser.createdAt}`);
+    console.log(`updated at: ${newUser.updatedAt}`);
+    console.log(`name: ${newUser.name}`);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import {
-    type CommandsRegistry,
+    CommandsRegistry,
     registerCommand,
     runCommand,
 } from "./commands/commands";
-import { handlerLogin } from "./commands/users";
+import { handlerLogin, handlerRegister } from "./commands/users";
 
-function main() {
-    const registry: CommandsRegistry = {}
-    registerCommand(registry, "login", handlerLogin);
-    
+async function main() {
     const args = process.argv.slice(2);
 
     if (args.length === 0) {
@@ -18,8 +15,12 @@ function main() {
 
     const cmdName = args[0];
     const cmdArgs = args.slice(1);
+    const commandsRegistry: CommandsRegistry = {}
+    registerCommand(commandsRegistry, "login", handlerLogin);
+    registerCommand(commandsRegistry, "register", handlerRegister);
+
     try {
-        runCommand(registry, cmdName, ...cmdArgs);
+        await runCommand(commandsRegistry, cmdName, ...cmdArgs);
     } catch (err) {
         if (err instanceof Error) {
             console.error(err.message);
@@ -29,6 +30,8 @@ function main() {
 
         process.exit(1);
     }
+
+    process.exit(0);
 }
 
 main();
