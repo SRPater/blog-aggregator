4c1fea3c3d0442d9f00396192c79f80204cf4be2
SRPater <srpater@gmail.com>
2025-08-22 12:26:12 +0200

    Finished CH4: Following, L1: Follow

---
+++ src/commands/feed-follows.ts
@@ -0,0 +1,58 @@
+import { getFeedByUrl } from "../lib/db/queries/feeds";
+import {
+    createFeedFollow,
+    getFeedFollowsForUser
+} from "../lib/db/queries/feed-follows";
+import { readConfig } from "../config";
+import { getUser } from "../lib/db/queries/users";
+
+export async function handlerFollow(cmdName: string, ...args: string[]) {
+    if (args.length !== 1) {
+        throw new Error(`usage: ${cmdName} <feed_url>`);
+    }
+
+    const config = readConfig();
+    const user = await getUser(config.currentUserName);
+
+    if (!user) {
+        throw new Error(`User ${config.currentUserName} not found`);
+    }
+
+    const feedUrl = args[0];
+    const feed = await getFeedByUrl(feedUrl);
+
+    if (!feed) {
+        throw new Error(`Feed not found: ${feedUrl}`);
+    }
+
+    const ffRow = await createFeedFollow(user.id, feed.id);
+    console.log("Feed follow created:");
+    printFeedFollow(ffRow.userName, ffRow.feedName);
+}
+
+export async function handlerListFeedFollows(_: string) {
+    const config = readConfig();
+    const user = await getUser(config.currentUserName);
+    
+    if (!user) {
+        throw new Error(`User ${config.currentUserName} not found`);
+    }
+
+    const feedFollows = await getFeedFollowsForUser(user.id);
+
+    if (feedFollows.length === 0) {
+        console.log("No feed follows found for this user.");
+        return;
+    }
+
+    console.log(`Feed follows for user %s:`, user.name);
+    
+    for (const ff of feedFollows) {
+        console.log(`* ${ff.feedName}`);
+    }
+}
+
+export function printFeedFollow(userName: string, feedName: string) {
+    console.log(`* User:  ${userName}`);
+    console.log(`* Feed:  ${feedName}`);
+}
